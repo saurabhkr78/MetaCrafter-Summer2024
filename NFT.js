b8c12fd7c36e79a25b51d928535200810c22d4a3
Saurabh Kumar <meetsaurabh78@gmail.com>
2024-06-02 16:58:02 +0530

    made Some Changes

--- NFT.js
+++ NFT.js
@@ -1,49 +1,46 @@
-// Array variable to hold NFTs
-//an empty array
+//every candidate is a unique digital asset i.e NFT
+// create Array variable to hold NFTs
+//Here i create an empty array
 //This array will store all the NFT objects we create.
 let nftCollection = [];
 
-
-// function for minting a new NFT and add it to our collection
+// function for minting a new NFT and add it to our collection i.e Array variable nftCollection
 function mintNFT(name, constituency, party) {
-    //Inside the function, we create an object nft
-    // A constant variable cannot be reassigned after its initial assignment, ensuring that the reference to the nft object remains unchanged.
+  //Inside the function, we create an object nft
+  // A constant variable cannot be reassigned after its initial assignment, ensuring that the reference to the nft object remains unchanged.
   const nft = {
     //three properties of NFT object
     name: name,
     constituency: constituency,
-    party: party
+    party: party,
   };
 
-//The new nft object is then added to the nftCollection array using the push method
+  //The new nft object is then added to the nftCollection array using the push method
   nftCollection.push(nft);
 }
 
-
-//to list all NFTs
+//function to list all NFTs
 function listNFTs() {
-    //use the forEach method to loop through each element (NFT) in the nftCollection array.
+  //use the forEach method to loop through each element (NFT) in the nftCollection array.
   nftCollection.forEach((nft, index) => {
-    
-    console.log(`NFT ${index + 1}:`);//to give each NFTs a number
+    console.log(`NFT ${index + 1}:`); //to give each NFTs a number and index=0(base)
     console.log(`  Name: ${nft.name}`);
     console.log(`  Constituency: ${nft.constituency}`);
     console.log(`  Party: ${nft.party}`);
   });
 }
 
-//to get the total number of NFTs
+//function to get the total number of NFTs
 function getTotalSupply() {
-  return nftCollection.length;//returns the length of the nftCollection array, which represents the number of NFTs in the collection.
+  return nftCollection.length; //returns the length of the nftCollection array, which represents the number of NFTs in the collection.
 }
 
-
 //call mintNFT three times with different MP details to create three NFTs.
-mintNFT("Narendra Modi", "Varanasi", "Bharatiya Janata Party");
-mintNFT("Rahul Gandhi", "Wayanad", "Indian National Congress");
-mintNFT("Mamata Banerjee", "Bhabanipur", "All India Trinamool Congress");
+mintNFT("Narendra Modi", "Varanasi", "BJP");
+mintNFT("Rahul Gandhi", "Rae Bareli", "INC");
+mintNFT("Mamata Banerjee", "Bhavanipur", "TMC");
 
-console.log("Listing all NFTs:");
-listNFTs();//to log all the NFTs in the collection
+console.log("The below following NFTs are minted:");
+listNFTs(); //to log all the NFTs in the collection
 
-console.log(`Total number of NFTs minted: ${getTotalSupply()}`);// total number of NFTs.
+console.log(`Total NFTs minted: ${getTotalSupply()}`); // total number of NFTs.
